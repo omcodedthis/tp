6f090d19ad6d6d803b9da60b42948d265129950e
oM <omtirodkar03@gmail.com>
2026-02-27 17:38:41 +0800

    Add SKU & SKUList classes

---
+++ src/main/java/SKU/SKU.java
@@ -0,0 +1,23 @@
+package SKU;
+
+import java.util.ArrayList;
+
+public class SKU {
+    private String skuID;
+    private Location skuLocation;
+    //private SKUTaskList skuTaskList;
+
+    public SKU(String skuID, Location skuLocation) {
+        this.skuID = skuID;
+        this.skuLocation = skuLocation;
+        // this.skuTaskList = new SKUTaskList();
+    }
+
+    public String getSKUID() {
+        return skuID;
+    }
+
+    public Location getSKULocation() {
+        return skuLocation;
+    }
+}
